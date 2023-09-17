0248b6526d01ab4dd32ce39ba6799e2271a5e355
Md. Wasikuzzaman Azad Sajol <wasik0003@gmail.com>
2023-09-17 20:51:25 +0600

    Update add-food.php

--- admin/add-food.php
+++ admin/add-food.php
@@ -1,5 +1,5 @@
-<?php include('../config/constants.php');
-	  include('login-check.php');
+<?php include('../frontend/config/constants.php');
+	  //include('login-check.php');
 	  error_reporting(0);
       @ini_set('display_errors', 0);
 
