0c3fd52bf5c023748f27f6f188292fc487036dc1
Md. Wasikuzzaman Azad Sajol <wasik0003@gmail.com>
2023-09-19 09:45:03 +0600

    Update inventory.php

--- admin/inventory.php
+++ admin/inventory.php
@@ -328,7 +328,7 @@ $row_message_notif = mysqli_num_rows($res_message_notif);
                                 //Image available. Displaying image
                                 ?>  <!-- Breaking PHP to write HTML -->
 
-                            <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>" width="100px">
+                            <img src="<?php echo SITEURL; ?>img/food/<?php echo $image_name; ?>" width="100px">
 
                             <?php
                             }
