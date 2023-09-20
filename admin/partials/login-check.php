11cb75e4de9516a50231b589f6709cc1fd3d8a62
Md. Wasikuzzaman Azad Sajol <wasik0003@gmail.com>
2023-09-20 11:17:53 +0600

    Update login-check.php

--- admin/partials/login-check.php
+++ admin/partials/login-check.php
@@ -8,7 +8,7 @@ if(!isset($_SESSION['user'])) //If user session is not set
     //Redirect to login page with message
 
     $_SESSION['no-login-message'] = "<div class='error text-center'>Please login to access Admin Panel</div>";
-    header('location:'.SITEURL.'admin/login.php');
+    header('location:'.SITEURL.'login.php');
 }
 
 
